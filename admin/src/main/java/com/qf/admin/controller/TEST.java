9ce6c0db2405719d037b93c09167534324d9365d
529428721 <529428721@qq.com>
2019-08-05 19:02:30 +0800

    新增商户功能

--- admin/src/main/java/com/qf/admin/controller/TEST.java
+++
@@ -1,9 +0,0 @@
-package com.qf.admin.controller;
-
-import com.qf.http.ResultCode;
-
-public class TEST {
-	public static void main(String[] args) {
-		System.out.println(ResultCode.get("金灿").getCode());
-	}
-}
